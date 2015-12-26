fef69a95f0160e5e5decf30d5f20fe65cab41e10
timkellogg <tim.kellogg@gmail.com>
2015-12-26 13:09:49 -0800

    refactor

--- app/helpers/convertDate.js
+++ app/helpers/convertDate.js
@@ -2,11 +2,3 @@ exports.convertDate = function(str) {
   var date = new Date(str);
   return date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
 };
-// module.exports = function formatDate(str) {
-//   var date = new Date(str);
-//   var str = "";
-//   str += (date.getMonth() + 1) + "/";
-//   str += date.getDate() + "/";
-//   str += date.getFullYear();
-//   return str;
-// }
