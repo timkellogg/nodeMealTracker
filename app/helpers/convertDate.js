27302d6f2a5ae973f2eb82e68290b160599ed167
timkellogg <tim.kellogg@gmail.com>
2015-12-27 20:47:51 -0800

    fix date errors

--- app/helpers/convertDate.js
+++ app/helpers/convertDate.js
@@ -1,9 +1,16 @@
+// Invalid date handler
+// Used because try...catch doesn't catch date errors
+Date.prototype.isValid = function() {
+  return this.getTime() === this.getTime();
+};
+
 exports.convertDate = function(str) {
   var date = new Date(str);
 
-  if (str !== '' && !isNaN(date)) {
-    return date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
-  } else {
-    return '';
-  }
+  
+  // if (str !== '' && !isNaN(date)) {
+  //   return date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear() + '';
+  // } else {
+  //   return '';
+  // }
 };
--- test/unit/convertDate.js
+++ test/unit/convertDate.js
@@ -4,7 +4,7 @@ var helpers = require('./../../app/helpers/convertDate');
 describe('the convertDate helper', function() {
   it('converts ISO string dates to "DD/MM/YY format"', function() {
     var date = 'Sun Dec 25 2016 00:00:00 GMT-0800 (PST)';
-    expect(helpers.convertDate(date)).to.equal('11/25/2016');
+    expect(helpers.convertDate(date)).to.equal('12/25/2016');
   });
 
   it('should return nothing for empty strings', function() {
@@ -17,4 +17,3 @@ describe('the convertDate helper', function() {
     expect(helpers.convertDate(date)).to.equal('');
   });
 });
-
